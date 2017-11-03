0ad6bc68912c3bc66714997df989d8b68952e71a
DESKTOP-G1UFRE1\sebas <sebastiaan.vandijk@hotmail.com>
2017-11-03 17:00:16 +0100

    Items and Inventory

---
+++ RPG/Assets/Scripts/Items/Item.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    new public string name = "New Item";        // Name of the item
+    public Sprite icon = null;                  // Item icon
+    public bool isDefaultItem = false;          // Is the item default wear?
+
+    public virtual void Use ()
+    {
+        Debug.Log("Using " + name);
+    }
+}
