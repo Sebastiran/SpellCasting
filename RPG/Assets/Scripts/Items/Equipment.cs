02bc78af764c6f2a3a6090858a90106d6444efc4
DESKTOP-G1UFRE1\sebas <sebastiaan.vandijk@hotmail.com>
2017-11-07 10:59:47 +0100

    Equipment and Equipment Mesh Intergration

---
+++ RPG/Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item
+{
+    public EquipmentSlot equipmentSlot; // Slot to store equipmnent in
+    public SkinnedMeshRenderer mesh;
+    public EquipmentMeshRegion[] coveredMeshRegions;
+
+    public int armorModifier;           // Increase/decrease in armor
+    public int damageModifer;           // Increase/decrease in damage
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);  // Equip it
+        RemoveFromInventory();                  // Remove it from inventory
+    }
+}
+
+public enum EquipmentSlot { Head, Chest, Legs, Weapon, Feet, Offhand }
+public enum EquipmentMeshRegion { Legs, Arms, Torso }; // Corresonds to body blendshapes.
