64b24ce80993d52ced5d8a5ae65c64428ab9cb2a
DESKTOP-G1UFRE1\sebas <sebastiaan.vandijk@hotmail.com>
2017-11-07 11:20:50 +0100

    Stats / Equipment Functionality

---
+++ RPG/Assets/Scripts/Stats/PlayerStats.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : CharacterStats
+{
+    private void Start()
+    {
+        EquipmentManager.instance.onEquipmentChanged += OnEquipmentChanged;
+    }
+
+    void OnEquipmentChanged(Equipment newItem, Equipment oldItem)
+    {
+        if (newItem != null)
+        {
+            armour.AddModifier(newItem.armorModifier);
+            damage.AddModifier(newItem.damageModifer);
+        }
+
+        if (oldItem != null)
+        {
+            armour.RemoveModifier(newItem.armorModifier);
+            damage.RemoveModifier(newItem.damageModifer);
+        }
+    }
+}
